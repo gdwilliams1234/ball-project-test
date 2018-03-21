fdd887e19a8855226deb558a4935f863b98afbb5
nitrogendragon <dirtboy4fun@gmail.com>
2018-03-20 23:47:36 -0500

    lots of stuff was added. made a mini game basically
    Added a timer, adjusted scoring system to make a way to lose at -50 points, added new scene that gets loaded when you lose the game, edited text displayed when winning game, changed win conditions from points to collided with cake at end of course, added an "obstacle" course, added some scripts to move things, added the loser scene and gave it an image to display and some music to be played.made some other various chagnes but I can't remember anymore what they were

--- Assets/scripts/PlayerControler.cs
+++ Assets/scripts/PlayerControler.cs
@@ -1,39 +1,114 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
-
+using UnityEngine.SceneManagement;
 
 public class PlayerControler : MonoBehaviour {
 
 	public float speed;
 	public Text countText;
+    public Text timertext;
 	public Text winText;
-    
-
-
+    bool isgrounded;
+    bool onramp;
+    Vector3 startingpoint;
+    public GameObject player;
     public int count;
 	private Rigidbody rb;
+    float timeout;
+    float timer=0;
+    bool starttimer = false;
+    GameObject cake;
+    bool timerstop = false;
 
 	void Start()
 	{
+        player = GameObject.Find("Cube");
+        startingpoint = new Vector3(0,5,0);
 		rb = GetComponent<Rigidbody>();
 		count = 0;
 		updateText (count);
+        cake = GameObject.Find("Cake");
+        
 	}
 
 	void FixedUpdate ()
 	{
-		float moveHorizontail = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+        if (timerstop == true)
+        {
+            timer = timer;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+        timertext.text = "Time: " + timer.ToString();
+        if (isgrounded == true || onramp == true)
+        {
+            float moveHorizontail = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontail, 0.0f, moveVertical);
+            Vector3 movement = new Vector3(moveHorizontail, 0.0f, moveVertical);
 
-		rb.AddForce (movement * speed);
-         
+            rb.AddForce(movement * speed);
+        }
+        if (transform.position.y <= -150)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            transform.position = startingpoint;
+        }
+        if(starttimer == true)
+        {
+            timeout += Time.deltaTime;
+        }
+        
+        if (timeout >= 5.0f)
+        {
+            print("should be changing");
+            SceneManager.LoadScene("loserscreen");
+        }
 
     }
 
-	void OnTriggerEnter(Collider other) {
+
+    //consider when character is jumping .. it will exit collision.
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isgrounded = false;   
+        }
+        if (collision.gameObject.CompareTag("ramp"))
+        {
+            onramp = false;
+        }
+    }
+
+    //checks if touching ground
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isgrounded = true;
+            
+        }
+        if (collision.gameObject.CompareTag("ramp"))
+        {
+            onramp = true;
+        }
+        if(collision.gameObject.CompareTag("cake"))
+        {
+            winText.text = "Congratulations!" +
+                " write down your time and try again" +
+                " you super hacker you!";
+            timerstop = true;
+        }
+    }
+
+   
+
+    void OnTriggerEnter(Collider other) {
 
 		if (other.gameObject.CompareTag ("pickup"))
 		{
@@ -46,9 +121,13 @@ public class PlayerControler : MonoBehaviour {
 
 	public void updateText(int counter){
 		countText.text = "Count: " + count.ToString ();
-		if (count >= 12)
+		
+        if(count ==-100)
 		{
-			winText.text = "You Win!";
+			winText.text = "Wasted!";
+            starttimer = true;  
 		}
+        
+        
 	}
 }
